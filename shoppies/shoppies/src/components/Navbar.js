068f07255b8d78da4233a5a40e6958c5f22adc3b
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-07 16:33:58 -0500

    components: add search button next to search on Navbar

--- shoppies/shoppies/src/components/Navbar.js
+++ shoppies/shoppies/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = (props) => {
   return (
     <div className="nb-container">
       <div className="search-bar">
-        <form className="search-form" onSubmit={props.fetchMovies}>
+        <form className="search-form" onSubmit={props.getMovies}>
           <input
             type="search"
             placeholder="Search movies"
@@ -12,6 +12,13 @@ const Navbar = (props) => {
             value={props.query}
             onChange={props.setMovieQuery}
           />
+          <button
+            className="button-search"
+            type="submit"
+            disabled={!props.query.length}
+          >
+            Search
+          </button>
         </form>
       </div>
     </div>
