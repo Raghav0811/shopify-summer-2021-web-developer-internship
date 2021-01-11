52ee9602ed4658f15206dd8baa58f593a0698134
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-11 11:12:07 -0500

    components: add button text change functionality on movie card

--- shoppies/shoppies/src/components/MovieCard.js
+++ shoppies/shoppies/src/components/MovieCard.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-const MovieCard = () => {};
+const MovieCard = (props) => {
+  const setButtonText = (movie) => {
+    return movie.nominated ? "Remove" : "Nominate";
+  };
+
+  return (
+    <div className="movie-card">
+      <div className="movie-img-container">
+        <img
+          src={props.movie.Poster}
+          alt={props.movie.Title}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = "";
+          }}
+        />
+      </div>
+      <p className="movie-title">{props.movie.Title}</p>
+      <p>{props.movie.Year}</p>
+      <button>{setButtonText(props.movie)}</button>
+    </div>
+  );
+};
 
 export default MovieCard;
